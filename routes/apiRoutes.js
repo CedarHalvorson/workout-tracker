008730f20f7e46e4980b63f74c4f0bfd8f2c255f
hat <cedarhalvorson@gmail.com>
2021-10-18 13:33:39 -0700

    finsished project

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Workout } = require(`../models/index`);
+const { create } = require('../models/workout');
 
 router.get (`/workouts`, async (req, res) => {
     try {
@@ -14,5 +15,32 @@ router.get (`/workouts`, async (req, res) => {
     }
 });
 
+router.post (`/workouts`, async (req, res) => {
+    try {
+        const createWorkout = await Workout.create({})
+        res.json(createWorkout)
+    }
+     catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.put ("/workouts/:id", async (req, res) => {
+    try {
+        Workout.findByIdAndUpdate(
+            req.params.id,
+            { $push: { exercises: req.body }}
+            ) .then(data => {
+               res.json(data);
+            })
+    }
+     catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+
 
 module.exports = router;
+
+
