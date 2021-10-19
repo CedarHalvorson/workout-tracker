ac9cc995cd72522b124be59b18c50961379f3b92
hat <cedarhalvorson@gmail.com>
2021-10-18 17:15:50 -0700

    server.js fixed

--- server.js
+++ server.js
@@ -1,31 +1,54 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const db = require('../models');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(`/`,routes);
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/limitless-dawn-11698', {
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+     useCreateIndex:true,
+     useFindAndModify: false
+});
 
-try {
-  mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/limitless-dawn-11698', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex:true,
-      useFindAndModify: false
-  }).then(() => {
-      console.log('Connected to mongodb database. :)')
-      app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`))
-    });
-  } catch (err) {console.log(err)};
+app.use(routes);
+app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`))
+
+
+// const express = require('express');
+// const path = require('path'); 
+// const routes = require('./routes');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+// // const db = require('../models');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(routes);
+
+// try {
+//   mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/limitless-dawn-11698', {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex:true,
+//       useFindAndModify: false
+//   }).then(() => {
+//       console.log('Connected to mongodb database. :)')
+//       app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`))
+//     });
+//   } catch (err) {console.log(err)};
 
 
   
